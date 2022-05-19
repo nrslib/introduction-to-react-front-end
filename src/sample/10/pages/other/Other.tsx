102f810840f36cacdc94d02ff69f610b84ac54a4
nrslib <masanobu.naruse@gmail.com>
2022-05-19 21:07:14 +0900

    update

--- src/sample/10/pages/other/Other.tsx
+++ src/sample/10/pages/other/Other.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Main from "../../../../components/layout/Main"
 import ButtonWithAgree from "../../components/ButtonWithAgree";
 import Link from "../../components/Link";
 
 export default function Other() {
+    const [agree, setAgree] = useState(false)
+
+    const handleAgreeChange = (value: boolean) => {
+        setAgree(value)
+    }
+
+    const handleButtonClick = () => {
+        alert("Other button clicked.")
+    }
+
     return (
         <Main>
             <h1>Other</h1>
-            <ButtonWithAgree/>
+            <ButtonWithAgree agree={agree} onAgreeChange={handleAgreeChange} onClick={handleButtonClick}/>
             <Link to="/sample/10">Index</Link>
         </Main>
     )
